5b1a8f076203720b7239f02962cb6e7060e67152
CS <syrious3000@yahoo.de>
2022-11-19 13:29:53 +0100

    traceId from sqs message restored on receive

--- src/main/java/org/acme/quartz/SqsSendReceive.java
+++ src/main/java/org/acme/quartz/SqsSendReceive.java
@@ -1,6 +1,12 @@
 package org.acme.quartz;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
+import io.opentelemetry.api.trace.TraceFlags;
+import io.opentelemetry.api.trace.TraceState;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.quarkus.logging.Log;
 import io.quarkus.scheduler.Scheduled;
@@ -9,12 +15,11 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.MDC;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
-import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
-import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
+import software.amazon.awssdk.services.sqs.model.*;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -32,6 +37,9 @@ public class SqsSendReceive {
     @ConfigProperty(name = "sqs.client.async")
     Boolean async;
 
+    private static final String TRACE_ID_KEY = "traceId";
+    private static final String SPAN_ID_KEY = "spanId";
+
 
     @Scheduled(every = "3s", identity = "send-job")
     public void send(){
@@ -42,10 +50,18 @@ public class SqsSendReceive {
     }
 
     private void sendSync() {
+        SpanContext spanContext = Span.current().getSpanContext();
+        String traceId = spanContext.getTraceId();
+        String spanId = spanContext.getSpanId();
+        Map<String, MessageAttributeValue> messageAttributes = Map.of(
+                TRACE_ID_KEY, MessageAttributeValue.builder().dataType("String").stringValue(traceId).build(),
+                SPAN_ID_KEY, MessageAttributeValue.builder().dataType("String").stringValue(spanId).build()
+        );
+
         SendMessageResponse response = sqs.sendMessage(m -> m
                 .queueUrl(queueUrl)
                 .messageBody("message" + UUID.randomUUID())
-                .messageGroupId("a")
+                .messageAttributes(messageAttributes)
         );
         Log.info("message sent\t\tID=%s".formatted(response.messageId()));
     }
@@ -69,21 +85,58 @@ public class SqsSendReceive {
         MDC.put("traceId", Span.current().getSpanContext().getTraceId());
         MDC.put("spanId", Span.current().getSpanContext().getSpanId());
         if(async) receiveAsync(); else receiveSync();
-        postReceive();
+        postReceive(); //with the traceId from this method we can see the original Sqs.ReceiveMessage span
     }
 
     private void receiveSync() {
-        sqs.receiveMessage(m -> m.maxNumberOfMessages(1).queueUrl(queueUrl)).messages().forEach(m -> {
-            //FIXME How to get MDCs updated in a less cumbersome/invasive way?
-            MDC.put("traceId", Span.current().getSpanContext().getTraceId());
-            MDC.put("spanId", Span.current().getSpanContext().getSpanId());
-            Log.info("message received\tID=%s".formatted(m.messageId()));
-            Log.info("message system attributes: %s".formatted(m.attributes().entrySet()
-                    .stream()
-                    .map(e -> e.getKey() + "=\"" + e.getValue() + "\"")
-                    .collect(Collectors.joining(", "))));
-            sqs.deleteMessage(DeleteMessageRequest.builder().queueUrl(queueUrl).receiptHandle(m.receiptHandle()).build());
+        sqs.receiveMessage(m -> m
+                .maxNumberOfMessages(1)
+                .queueUrl(queueUrl)
+                .messageAttributeNames(TRACE_ID_KEY, SPAN_ID_KEY)
+        ).messages().forEach(m -> {
+            String traceId = m.messageAttributes().get(TRACE_ID_KEY).stringValue();
+            String spanId = m.messageAttributes().get(SPAN_ID_KEY).stringValue();
+
+            //restore remote trace
+            Span span = createSpanLinkedToParent(traceId, spanId);
+            try (Scope scope = span.makeCurrent()) {
+                //FIXME How to get MDCs updated in a less cumbersome/invasive way?
+                MDC.put("traceId", Span.current().getSpanContext().getTraceId());
+                MDC.put("spanId", Span.current().getSpanContext().getSpanId());
+                Log.info("message received\tID=%s".formatted(m.messageId()));
+                Log.info("message system attributes: %s".formatted(m.attributes().entrySet()
+                        .stream()
+                        .map(e -> e.getKey() + "=\"" + e.getValue() + "\"")
+                        .collect(Collectors.joining(", "))));
+
+                deleteMessage(m);
+                postReceive();
+            } finally {
+                span.end();
+            }
         });
+    }
+
+    @WithSpan("deleteMessage")
+    private void deleteMessage(Message m) {
+        //FIXME How to get MDCs updated in a less cumbersome/invasive way?
+        MDC.put("traceId", Span.current().getSpanContext().getTraceId());
+        MDC.put("spanId", Span.current().getSpanContext().getSpanId());
+        sqs.deleteMessage(DeleteMessageRequest.builder().queueUrl(queueUrl).receiptHandle(m.receiptHandle()).build());
+    }
+
+    private static Span createSpanLinkedToParent(String traceId, String spanId) {
+        // Fetch the trace and span IDs from wherever you've stored them
+        SpanContext remoteContext = SpanContext.createFromRemoteParent(
+                traceId,
+                spanId,
+                TraceFlags.getSampled(),
+                TraceState.getDefault());
+
+        return GlobalOpenTelemetry.getTracer("")
+                .spanBuilder("Sqs.ReceiveMessage")
+                .setParent(Context.current().with(Span.wrap(remoteContext)))
+                .startSpan();
     }
 
     private void receiveAsync() {
